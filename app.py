b247ff6f538b7eaae69a1d14d7d3a1eeffa548f3
tpssoni <tapassoni5@gmail.com>
2020-02-05 18:00:29 +0530

    first commit

--- app.py
+++ app.py
@@ -5,6 +5,16 @@ from database import connect_db, get_db
 
 app = Flask(__name__)
 
+def connect_db():
+	sql = sqlite3.connect('/home/ubuntu/nutrition_app/food_log.db')
+	sql.row_factory = sqlite3.Row
+	return sql
+
+def get_db():
+	if not hasattr(g, 'sqlite3_db'):
+		g.sqlite_db = connect_db()
+	return g.sqlite_db
+
 
 @app.teardown_appcontext
 def close_db(error):
--- database.py
+++ database.py
@@ -1,12 +1,12 @@
-from flask import g
-import sqlite3
+# from flask import g
+# import sqlite3
 
-def connect_db():
-	sql = sqlite3.connect('/home/ubuntu/nutrition_app/food_log.db')
-	sql.row_factory = sqlite3.Row
-	return sql
+# def connect_db():
+# 	sql = sqlite3.connect('/home/ubuntu/nutrition_app/food_log.db')
+# 	sql.row_factory = sqlite3.Row
+# 	return sql
 
-def get_db():
-	if not hasattr(g, 'sqlite3_db'):
-		g.sqlite_db = connect_db()
-	return g.sqlite_db
+# def get_db():
+# 	if not hasattr(g, 'sqlite3_db'):
+# 		g.sqlite_db = connect_db()
+# 	return g.sqlite_db
